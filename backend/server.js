2ad0a3065c7f1a5f31b4a2754ec69c46888bbeeb
Kaan <kaanasutay10@gmail.com>
2024-10-19 17:46:26 +0300

    add gitignore

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+"use strict"
+/* -------------------------------------------------------
+    NODEJS EXPRESS | Autentication Tutorial
+------------------------------------------------------- */
+
+const server = () => {
+    console.log('server is running...')
+};
+
+server();
