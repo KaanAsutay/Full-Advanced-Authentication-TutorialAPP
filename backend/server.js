3f677159f9cde53bacbccefefe3ebc29f8daea7e
Kaan <kaanasutay10@gmail.com>
2024-10-19 17:58:04 +0300

    create a middleware and required modules in the server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,44 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | Autentication Tutorial
 ------------------------------------------------------- */
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
-const server = () => {
-    console.log('server is running...')
+const app = express();
+
+/* ------------------------------------------------------- */
+// Required Modules:
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// Middlewares:
+
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    credentials: true
+}))
+
+// Accept JSON:
+app.use(express.json())
+
+app.use(express.urlencoded({ extended: true }))
+
+
+
+
+const server = async () => {
+    try {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (error) {
+        console.log("Failed to start server...", error.message)
+        process.exit(1)
+    }
 };
 
 server();
