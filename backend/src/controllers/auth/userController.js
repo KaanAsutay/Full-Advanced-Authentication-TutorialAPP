d64deaa9422809469b58f214d32ed983e6a343a7
Kaan <kaanasutay10@gmail.com>
2024-10-21 18:17:08 +0300

    create Logout user

--- backend/src/controllers/auth/userController.js
+++ backend/src/controllers/auth/userController.js
@@ -125,3 +125,10 @@ export const loginUser = asyncHandler(async (req, res) => {
         res.status(400).json({ message: "Invalid email or password " })
     }
 })
+
+// Logout user
+export const logoutUser = asyncHandler(async (req, res) => {
+    res.clearCookie("token");
+
+    res.status(200).json({ message: "User logged out" })
+})
