7a954b95083a5abf5192d1982085d349cb69e5f2
Kaan <kaanasutay10@gmail.com>
2024-10-23 23:26:40 +0300

    attempt to find and delete the user

--- backend/src/controllers/auth/adminController.js
+++ backend/src/controllers/auth/adminController.js
@@ -3,7 +3,25 @@
     NODEJS EXPRESS | Authentication Tutorial
 ------------------------------------------------------- */
 import asyncHandler from "express-async-handler"
+import User from "../../models/auth/UserModel";
 
 export const deleteUser = asyncHandler(async (req, res) => {
-    req.status(200).json({ message: "User deleted" })
+    const {id} = req.params;
+
+    // attempt to find and delete the user
+    try {
+    const user = await User.findByIdAndDelete(id);
+
+    // check if user exists
+    if (!user) {
+        res.status(404).json({ message: "User not found!" })
+    }
+
+    // delete user
+    res.status(200).json({ message: "User deleted successfully!"})
+        
+    } catch (error) {
+        res.status(500).json({ message: "Cannot delete user" })
+        
+    }
 });
